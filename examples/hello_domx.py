7cada5f8f5cb13493504adbe9bcf4dfe74660fda
Tom Flanagan <tom@zkpq.ca>
2022-03-13 18:35:45 -0700

    finish dx

--- examples/hello_domx.py
+++ examples/hello_domx.py
@@ -5,8 +5,21 @@ from dominate.tags import *
 
 class Hello(dx.Page):
   @div
-  def hello():
+  def index(self):
     h1('Hello world')
+    button('foo', dx.x(target='#content', get='/foo'))
+    button('bar', dx.x(target='#content', get='/bar'))
+    with div(id='content'):
+      self.foo()
+
+  @div
+  def foo(self):
+    h3('asdf asdf')
+
+  @div
+  def bar(self):
+    h3('lorem ipsum')
+
 
 
 if __name__ == '__main__':
