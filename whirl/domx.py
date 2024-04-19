a69e1087bc7d225ca9d4c72c3ad7d78e985124bc
Tom Flanagan <tom@zkpq.ca>
2024-04-18 20:43:27 -0700

    lolidk

--- whirl/domx.py
+++ whirl/domx.py
@@ -5,12 +5,18 @@ import pathlib
 import re
 import socketserver
 import sys
+import typing
 import traceback
+import json
 
 import dominate
 import toml
 from dominate import tags, util
 
+try:
+  from IPython.core import ultratb
+except ImportError:
+  ultratb = None
 
 import whirl
 # from whirl import domx_server
@@ -40,18 +46,25 @@ class DomxServer(
   template_cls = dominate.document
   routes = []
 
+  class Path(typing.NamedTuple):
+    q: str
+    cb: typing.Callable
+    type: str
+
   @classmethod
   def template(cls, t):
     cls.template_cls = t
     return t
 
   @classmethod
-  def route(cls, route, cb=None):
-    if cb:
-      cls.routes.append((route, cb))
+  def route(cls, route, *args, type=None, **kw):
+    if args:
+      # normal call
+      cls.routes.append(DomxServer.Path(route, args[0], type, **kw))
     else:
+      # decorator with args
       def add(cb):
-        cls.routes.append((route, cb))
+        cls.routes.append(DomxServer.Path(route, cb, type, **kw))
         return cb
       return add
 
@@ -69,13 +82,13 @@ class DomxServer(
     httpd.timeout = 0.1
     httpd.server_bind()
     httpd.server_activate()
-    log.info('Serving at {}'.format(addr))
+    log.info(f'Serving at {addr}')
     try:
       while 1:
         try:
           # httpd.serve_forever() # poll_timeout doesnt work
           httpd.handle_request()
-        except TimeoutError as e:
+        except TimeoutError:
           pass
         except KeyboardInterrupt as e:
           log.info(f'Stopping server: {e!r}')
@@ -112,6 +125,7 @@ class DomxServer(
     host = self.headers.get('Host', '')
     url = whirl.url(self.path)
 
+    # TODO get rid of this convention and clean up request type handling
     xhr = False
     parts = list(url.path.parts)
     if len(parts) > 1 and parts[1] == 'domx':
@@ -121,49 +135,63 @@ class DomxServer(
       xhr = True
 
     r = 200
-    cb, *args = self.match(str(url.path))
-    if not cb:
-      r = 404
-      d = self.page_error(r, 'Page Not Found')
-    else:
-      try:
+    content_type = 'text/html'
+
+    try:
+      route, match = self.match(str(url.path))
+      if not route:
+        raise http.HTTPStatus(404)
+
+
+      if route.type == 'json':
+        d = json.dumps(route.cb(url, self, *match.groups()), sort_keys=True, indent='  ')
+        content_type = 'application/json'
+
+      else:
         with container() as c:
-          d = cb(url, self, *args)
+          d = route.cb(url, self, *match.groups())
         if not d:
           d = c
         self.domxify(d)
-      except Exception as e:
-        log.exception('Internal Server Error')
-        r = 500
-        d = self.page_error(r, 'Internal Server Error',
-          tags.div(
-            tags.h1(type(e).__name__ + ': ' + str(e)),
-            tags.pre(traceback.format_exc())))
 
-    if not xhr:
-      if not isinstance(d, dominate.document):
-        doc = self.template_cls(str(url))
-        doc += d
-        d = doc
-      d.head += tags.script(util.include(static_root / 'domx' / 'domx.js'))
-      d.head += tags.style( util.include(static_root / 'domx' / 'domx.css'))
+        if not xhr: # wrap d in the page template
+          if not isinstance(d, dominate.document):
+            doc = self.template_cls(str(url))
+            doc += d
+            d = doc
+          d.head += tags.script(util.include(static_root / 'domx' / 'domx.js'))
+          d.head += tags.style( util.include(static_root / 'domx' / 'domx.css'))
+
+    except Exception as e:
+      r = 500
+      with tags.div() as tb:
+          if ultratb:
+            vb = ultratb.VerboseTB()
+            dominate.util.text(vb.structured_traceback())
+          else:
+            tags.h1(type(e).__name__ + ': ' + str(e)),
+            tags.pre(traceback.format_exc())
+
+      d = self.page_error(r, tb)
+
 
     self.send_response(r)
-    self.send_header('Content-Type', 'text/html; charset=utf-8')
+    self.send_header('Content-Type', f'{content_type}; charset=utf-8')
     self.end_headers()
-    self.wfile.write(d.render().encode('utf-8'))
+    self.wfile.write(str(d).encode('utf-8'))
 
   def match(self, path):
-    for q, cb in self.routes:
-      if type(q) is str:
-        m = re.match(q + '$', str(path))
-        if not m: continue
-        return cb, m
-    return (None,)
+    for route in self.routes:
+      m = re.match(route.q + '$', str(path))
+      if not m: continue
+      return route, m
+    return None, None
 
 
-  def page_error(self, code, msg='', body=None):
-    d = self.template_cls(f'{code} {msg}')
+  def page_error(self, code, body=None):
+    s = http.HTTPStatus(code)
+    msg = f'{code} {s.phrase} {s.description}'
+    d = self.template_cls(msg)
     d += body or msg
     return d
 
@@ -181,14 +209,14 @@ class DomxServer(
           path = i['post']
         else:
           raise ValueError('no domx method specified')
-        outer =  int(bool(i.attributes.get('outer')))
-        target = i.attributes.get('target', 'this')
-        event = i.attributes.get('event', 'onclick')
-        before = i.attributes.get('before')
-        after = i.attributes.get('after')
+        outer   =  int(bool(i.attributes.get('outer')))
+        target  = i.attributes.get('target', 'this')
+        event   = i.attributes.get('event', 'onclick')
+        before  = i.attributes.get('before')
+        after   = i.attributes.get('after')
 
-        before = f"(function(){{{before}}})" if before else 'null'
-        after = f"(function(){{{after}}})" if after else 'null'
+        before  = f"(function(){{{before}}})" if before else 'null'
+        after   = f"(function(){{{after}}})" if after else 'null'
 
         # todo onsubmit, etc for different types
         node[event] = (
@@ -200,4 +228,3 @@ class DomxServer(
         )
         node.children.remove(i)
       self.domxify(i)
-
