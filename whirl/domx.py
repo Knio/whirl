34f824922c9ae2d4b7c60e7c21242bfc0717784a
Tom Flanagan <tom@zkpq.ca>
2024-11-28 22:02:07 -0800

    nice tracebacks

--- whirl/domx.py
+++ whirl/domx.py
@@ -1,22 +1,21 @@
 import argparse
 import http.server
+import io
+import json
 import logging
 import pathlib
 import re
 import socketserver
-import sys
-import typing
 import traceback
-import json
+import typing
 
 import dominate
-import toml
 from dominate import tags, util
 
 try:
-  from IPython.core import ultratb
+  import rich.traceback
 except ImportError:
-  ultratb = None
+  rich = None
 
 from .url import url
 
@@ -35,12 +34,19 @@ class dx(tags.dom_tag):
   pass
 
 
+class HTTPError(RuntimeError):
+  @classmethod
+  def fromcode(cls, x):
+    return cls(http.HTTPStatus(x))
+
+
 # this is exported as whirl.domx
 class DomxServer(
     socketserver.ThreadingMixIn,
     http.server.BaseHTTPRequestHandler):
   dx = dx
   container = container
+  httperror = HTTPError.fromcode
 
   template_cls = dominate.document
   routes = []
@@ -139,8 +145,7 @@ class DomxServer(
     try:
       route, match = self.match(str(u.path))
       if not route:
-        raise http.HTTPStatus(404)
-
+        raise HTTPError.fromcode(404)
 
       if route.type == 'json':
         d = json.dumps(route.cb(u, self, *match.groups()), sort_keys=True, indent='  ')
@@ -163,13 +168,77 @@ class DomxServer(
 
     except Exception as e:
       r = 500
+      if isinstance(e, HTTPError):
+        e = e.args[0]
+        phrase = e.phrase
+        description = e.description
+      else:
+        r = 500
+        phrase = type(e).__name__ + ': ' + str(e)
+        description = ''
+
       with tags.div() as tb:
-          tags.h1(type(e).__name__ + ': ' + str(e)),
-          if ultratb and False:
-            # TODO this is not useful
-            vb = ultratb.VerboseTB()
-            lines = vb.structured_traceback(type(e), e)
-            tags.pre('\n\n'.join(lines))
+          tags.h1(phrase)
+          tags.p(description)
+          if rich:
+            # rtb = rich.traceback.Traceback()
+            out = io.StringIO()
+            con = rich.console.Console(
+              file=out,
+              record=True,
+              width=200,
+            )
+            con.print_exception(
+                show_locals=True,
+                width=200,
+            )
+            util.raw(con.export_svg(code_format='''
+              <svg class="rich-terminal" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg">
+                  <style>
+                    @font-face {{
+                        font-family: "Fira Code";
+                        src: local("FiraCode-Regular"),
+                                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff2/FiraCode-Regular.woff2") format("woff2"),
+                                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff/FiraCode-Regular.woff") format("woff");
+                        font-style: normal;
+                        font-weight: 400;
+                    }}
+                    @font-face {{
+                        font-family: "Fira Code";
+                        src: local("FiraCode-Bold"),
+                                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff2/FiraCode-Bold.woff2") format("woff2"),
+                                url("https://cdnjs.cloudflare.com/ajax/libs/firacode/6.2.0/woff/FiraCode-Bold.woff") format("woff");
+                        font-style: bold;
+                        font-weight: 700;
+                    }}
+                    .{unique_id}-matrix {{
+                        font-family: Fira Code, monospace;
+                        font-size: {char_height}px;
+                        line-height: {line_height}px;
+                        font-variant-east-asian: full-width;
+                    }}
+                    .{unique_id}-title {{
+                        font-size: 18px;
+                        font-weight: bold;
+                        font-family: arial;
+                    }}
+                    {styles}
+                  </style>
+
+                  <defs>
+                    <clipPath id="{unique_id}-clip-terminal">
+                      <rect x="0" y="0" width="{terminal_width}" height="{terminal_height}" />
+                    </clipPath>
+                    {lines}
+                  </defs>
+
+                  <g transform="translate({terminal_x}, {terminal_y})" clip-path="url(#{unique_id}-clip-terminal)">
+                  {backgrounds}
+                    <g class="{unique_id}-matrix">
+                      {matrix}
+                    </g>
+                  </g>
+              </svg>'''))
           else:
             tags.pre(traceback.format_exc())
 
